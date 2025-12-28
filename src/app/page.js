5eeb20078784af09db6dd8708ebde85721b34632
Ahmed Missouri <ahmedmissouri@icloud.com>
2025-12-28 07:51:56 +0000

    init project

---
+++ src/app/page.js
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="relative w-full h-screen overflow-hidden bg-white dark:bg-black text-black dark:text-white">
+      {/* Art Trail will be rendered here */}
+    </main>
+  );
+}
---
+++ src/lib/constants.js
@@ -0,0 +1,8 @@
+// API Configuration
+export const ARTIC_API_BASE_URL = 'https://api.artic.edu/api/v1';
+export const IIIF_BASE_URL = 'https://www.artic.edu/iiif/2';
+
+// Trail Configuration
+export const TRAIL_BUFFER_SIZE = 50;
+export const MOUSE_MOVE_THRESHOLD = 50;
+export const IMAGE_WIDTH = 400;
